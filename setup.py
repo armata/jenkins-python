aad9017e9b598a0e4a38bd5bedc42360712f1c54
Sebastian Malek <sebastian.malek.ets@gmail.com>
2016-08-29 19:25:10 +0200

    add files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='jenkins-python',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/sebastian-malek/jenkins-python',
+    license='',
+    author='Sebastian Malek',
+    author_email='sebastian.malek.ets@gmail.com',
+    description='',
+    py_modules=['jenkins']
+)

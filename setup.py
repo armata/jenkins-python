64ecc2242741aec071ceb081a2acb1557548b6ab
Sebastian Malek <sebastian.malek.ets@gmail.com>
2016-08-29 19:27:12 +0200

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from distutils.core import setup
 setup(
     name='jenkins-python',
     version='1.0',
-    packages=[''],
     url='https://github.com/sebastian-malek/jenkins-python',
     license='',
     author='Sebastian Malek',

6e39bcbe7bd048af85020d7a8e05b185c43ff9e5
Sebastian Malek <malek@malek.li>
2019-05-29 12:20:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name='jenkins-python',
+    name='jenkinshash',
     version='1.1',
-    url='https://github.com/sebastian-malek/jenkins-python',
+    url='https://github.com/sebastian-malek/jenkinshash',
     license='',
     author='Sebastian Malek',
-    author_email='sebastian.malek.ets@gmail.com',
+    author_email='malek@malek.li',
     description='',
     py_modules=['jenkins']
 )
